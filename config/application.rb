268fe6d53c1af04e262efdeb127f4616078af68d
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-08 15:53:49 +0900

    feat:タイムゾーンを日本に設定、rails g コマンドで余計なファイルを自動生成しない設定を追加

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,16 @@ Bundler.require(*Rails.groups)
 module Reminiscence
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    
     config.load_defaults 6.1
-
+    
+    config.generators do |g|
+      # この二行の記述で自動生成しない設定を作成しています。
+      g.assets false
+      g.helper false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
