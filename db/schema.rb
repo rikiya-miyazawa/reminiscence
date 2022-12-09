5c85254c851a4f505f455508ca1d80fda593c118
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-09 10:27:56 +0900

    feat:ユーザ登録機能を実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_08_130216) do
+ActiveRecord::Schema.define(version: 2022_12_09_001012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_12_08_130216) do
     t.text "user_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
