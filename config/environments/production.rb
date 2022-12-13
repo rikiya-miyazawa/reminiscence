4ceee288099495c5dfc53f0031ee1d4ed3b66394
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-13 15:46:11 +0900

    feat:本番環境にメール送信機能を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,18 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://shrouded-plains-13491.herokuapp.com/' }
   config.action_mailer.delivery_method = :letter_opener_web
 
+  config.action_mailer.default_url_options = { host: 'shrouded-plains-13491.herokuapp.com' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: "smtp.sendgrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
