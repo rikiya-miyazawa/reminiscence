403fa1c9fdafea8ac28ccb9d13ac86fe170f678d
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-13 16:04:06 +0900

    feat:本番環境にメール送信機能を追加

--- app/mailers/picture_mailer.rb
+++ app/mailers/picture_mailer.rb
@@ -2,6 +2,6 @@ class PictureMailer < ApplicationMailer
   def picture_mail(picture)
     @picture = picture
 
-    mail to: @picture.user.email, subject: "投稿確認メール"
+    mail to: "rky.weed.rss@gmail.com", subject: "投稿確認メール"  #mail  to: @picture.user.email 
   end
 end
