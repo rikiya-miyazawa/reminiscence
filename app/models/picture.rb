216c7e402b4fbbca493cb2aab25050db7fc3c180
rikiya miyazawa <rky.weed.rss@gmail.com>
2022-12-13 13:47:31 +0900

    init

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_many :favorites, dependent: :destroy
   has_many :favorite_users, through: :favorites, source: :user
   validates :image, presence: true
